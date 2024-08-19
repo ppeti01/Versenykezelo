f995f09d524247dc9ecc3af59a7c407edb202ba3
Patyi Péter <patyipeti01@gmail.com>
2024-08-19 16:01:04 +0200

    Fordulók tábla létrehozzása

---
+++ tournament_manager_app/database/migrations/2024_08_19_135259_create_rounds_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rounds', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('competition_id');
+            $table->foreign('competition_id')->references('id')->on('competitions')->onDelete('cascade');
+            $table->string('name');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rounds');
+    }
+};
